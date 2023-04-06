dd8aefb162e777b7c476804f42433566ac13b57a
PedroAC18 <pedro.correa1101@gmail.com>
2023-04-06 15:41:24 -0300

    Exercício Herança

--- Herança e Polimorfismo/Ex01/src/application/Program.java
+++ Herança e Polimorfismo/Ex01/src/application/Program.java
@@ -1,4 +1,43 @@
 package application;
 
+import entities.Employee;
+import entities.OutsorcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> listaFun = new ArrayList<>();
+
+        System.out.print("Entre com o numero de funcionarios: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.print("Terceirizado (s/n) ?");
+            sc.nextLine();
+            String terc = sc.nextLine();
+
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+            System.out.print("Horas: ");
+            int horas = sc.nextInt();
+            System.out.print("Valor hora: ");
+            double valH = sc.nextDouble();
+
+            if(terc.equals("s")){
+                System.out.print("Custo adicional: ");
+                double custo = sc.nextDouble();
+                listaFun.add(new OutsorcedEmployee(nome, horas, valH, custo));
+            }else{
+                listaFun.add(new Employee(nome, horas, valH));
+            }
+        }
+
+        for (Employee emp :listaFun ){
+            System.out.println("Nome: " + emp.getName() + " - $"+ emp.payment());
+        }
+
+    }
 }
